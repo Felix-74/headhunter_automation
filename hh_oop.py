cb5df60ab4bb74df0a39b4983910aa4c2c712e81
Игорь Теплов <2173045@mail.ru>
2024-07-01 15:43:57 +0500

    example.env file added

--- hh_oop.py
+++ hh_oop.py
@@ -7,6 +7,7 @@ from dotenv import load_dotenv
 # Загрузите переменные окружения из .env файла
 load_dotenv()
 
+
 class HHAPI:
     def __init__(self):
         self.access_token = os.getenv('ACCESS_TOKEN')
@@ -20,7 +21,8 @@ class HHAPI:
         self.vacancies_ids = []
 
     def get_resume_list(self):
-        response = requests.get('https://api.hh.ru/resumes/mine', headers=self.headers)
+        response = requests.get('https://api.hh.ru/resumes/mine',
+                                headers=self.headers)
         resume_list = response.json().get('items', [])
         if not resume_list:
             print("Резюме не найдено")
@@ -31,7 +33,8 @@ class HHAPI:
             print("Using Resume ID:", self.resume_id)
             return resume_list
 
-    def search_vacancies(self, search_row, area=113, period=3000, per_page=100, experience='between1And3'):
+    def search_vacancies(self, search_row, area=113, period=3000,
+                         per_page=100, experience='between1And3'):
         params = {
             'text': search_row,
             'area': area,
@@ -40,7 +43,8 @@ class HHAPI:
             'experience': experience
         }
         vacancy_url = 'https://api.hh.ru/vacancies'
-        response = requests.get(vacancy_url, headers=self.headers, params=params)
+        response = requests.get(vacancy_url, headers=self.headers,
+                                params=params)
         vacancies = response.json()
         self.vacancies_ids = [x['id'] for x in vacancies['items']]
         return self.vacancies_ids
@@ -58,7 +62,11 @@ class HHAPI:
             }
             click_url = 'https://api.hh.ru/negotiations'
             try:
-                response = requests.post(click_url, headers=self.headers, params=params)
+                response = requests.post(
+                    click_url,
+                    headers=self.headers,
+                    params=params
+                )
                 k += 1
                 print("отлик +1")
                 time.sleep(3)
@@ -66,13 +74,13 @@ class HHAPI:
                 print(f'Возникло исключение: {e}')
                 print(f'отправлено {k} откликов')
 
+
 if __name__ == "__main__":
     hh_api = HHAPI()
 
     # Получение списка резюме
     hh_api.get_resume_list()
 
-    # Поисковый запрос
     search_row = 'NAME:("python" OR "python программист" NOT "senior ")'
 
     # Поиск вакансий
